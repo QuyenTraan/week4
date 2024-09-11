ccc0b5a8a6c72c5da2835c439db68404af8228d7
Quyên <Quyen.TranVoTo@scc.com>
2024-09-11 11:05:55 +0700

    Test only

--- ass4/components/__tests__/Test.js
+++ ass4/components/__tests__/Test.js
@@ -5,6 +5,6 @@ export function MonoText(props) {
   return (
     <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />
   );
-  let date = Quyen TextTrackList();
+  let date = Date();
   
 }
